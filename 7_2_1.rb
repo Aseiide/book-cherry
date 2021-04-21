ac061a46a8a24273a4efccaba99f9147dfc44990
Aseiide <eiseiide@gmail.com>
2021-04-21 12:37:35 +0900

    クラスを自作

--- 7_2_1.rb
+++ 7_2_1.rb
@@ -1,16 +1,22 @@
+class User
+  attr_reader :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
+
 users = []
-users << { first_name: "Alice", last_name: "Ruby", age: 20}
-users << { first_name: "Bob", last_name: "Python", age: 30}
+users << User.new("Alice", "Ruby", 20) 
+users << User.new("Bob", "Python", 30)
 
-users[0][:first_name] = "Carol"
-# 氏名を作成するメソッド
-def full_name(user)
-  "#{user[:first_name]} #{user[:last_name]}"
-end
 
-# ユーザーのデータｗｐ表示する
 users.each do |user|
-  puts "氏名: #{full_name(user)} 年齢: #{user[:age]}"
+  puts "氏名: #{user.full_name} 年齢: #{user.age}"
 end
-
-puts users[0]
